b8064f40e8c67d81723d102e49bc247571f22fc9
Half's Inner <kajbrz@hotmail.com>
2020-03-13 02:36:15 +0100

    Initial Commit

---
+++ MLPApproximator/MlpAssembly.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+
+class MlpApproximator:
+    def run(self):
+        return np.array([1, 2, 3])
---
+++ main.py
@@ -0,0 +1,5 @@
+from MLPApproximator.MlpAssembly import MlpApproximator
+
+if __name__ == "__main__":
+    mlp = MlpApproximator()
+    print(mlp.run())
