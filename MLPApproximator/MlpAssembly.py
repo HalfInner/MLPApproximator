34e7bedca89e015bdc5df4194ba0e9b1ace214e8
Half's Inner <kajbrz@hotmail.com>
2020-03-26 21:56:28 +0100

    Add function generator

--- MLPApproximator/MlpAssembly.py
+++ MLPApproximator/MlpAssembly.py
@@ -6,9 +6,59 @@ import numpy as np
 from MLPApproximator.MlpApproximator import MlpApproximator
 
 
+class FunctionGenerator:
+
+    def __init__(self) -> None:
+        self.__function_store = []
+        pass
+
+    def addFunction(self, polynomial):
+        """
+
+        :param polynomial: group of consecutive polynomial
+            e.g. [2, 3, 1] > 2x^2 + 3x^1 + 1
+        """
+        self.__function_store.append(polynomial)
+
+    def generate(self, samples_number):
+        pass
+
+    def to_string(self):
+        output_str = ""
+        for polynomial in self.__function_store:
+            step = -1
+            for factor in reversed(polynomial):
+                step += 1
+                if factor == 0:
+                    continue
+
+                local_str = ' + ' + str(factor)
+                if step == 1:
+                    local_str += 'x'
+                if step > 1:
+                    local_str += 'x^' + str(step)
+                output_str = local_str + output_str
+            output_str = output_str.replace(' + ', '', 1).lstrip()
+            output_str += "\n"
+
+        return output_str
+
+
 class MlpApproximatorAssembler:
     """
     Assembly modules from the project
+
+    example::
+        def run(self) -> str:
+            first_sample = np.array([1, 2]).reshape((2, 1))
+            expected_out = np.array([1, 0]).reshape((2, 1))
+
+            mlp_approximator = MlpApproximator(2, 2, 3)
+            mlp_approximator.propagateForward(first_sample)
+            mlp_approximator.propagateErrorBackward(expected_out)
+
+            return 0
+         ``
     """
 
     def run(self) -> str:
@@ -19,7 +69,7 @@ class MlpApproximatorAssembler:
         first_sample = np.array([1, 2]).reshape((2, 1))
         expected_out = np.array([1, 0]).reshape((2, 1))
 
-        mlp_approximator = MlpApproximator(2, 2)
+        mlp_approximator = MlpApproximator(2, 2, 3)
         mlp_approximator.propagateForward(first_sample)
         mlp_approximator.propagateErrorBackward(expected_out)
 
---
+++ MLPApproximator/test_functionGenerator.py
@@ -0,0 +1,24 @@
+#  Copyright (c) 2020
+#  Kajetan Brzuszczak
+
+from unittest import TestCase
+
+from MLPApproximator.MlpAssembly import FunctionGenerator
+
+
+class TestFunctionGenerator(TestCase):
+    def test_addOneFunction(self):
+        function_generator = FunctionGenerator()
+        function_generator.addFunction([10, 4, 3])
+
+        expected_output = "10x^2 + 4x + 3\n"
+        self.assertEqual(expected_output, function_generator.to_string())
+
+    def test_addTwoFunctions(self):
+        function_generator = FunctionGenerator()
+        function_generator.addFunction([10, 4, 3])
+        function_generator.addFunction([-5, 4, 3])
+
+        expected_output = "10x^2 + 4x + 3" \
+                          "-5x^2 + 4x + 3"
+        self.assertEqual(expected_output, function_generator.to_string())
