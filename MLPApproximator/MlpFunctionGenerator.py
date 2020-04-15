Fix function generator Introduce Auto Normalization

MLPApproximator/MlpFunctionGenerator.py
@@ -47,18 +47,23 @@ class FunctionGenerator:
         self.__function_store.append(polynomial)
 
     def generate(self, samples_number=1):
-        input_set = np.linspace(-1., 1., samples_number, dtype=float)
+        if samples_number <= 0:
+            raise ValueError('Number of samples must be greater than 0')
 
+        input_set = np.linspace(-1., 1., samples_number, dtype=float)
         output_set = np.empty((0, samples_number), dtype=float)
         for idx, polynomial in enumerate(self.__function_store):
             f_x = lambda x: sum([factor * x ** step for step, factor in enumerate(reversed(polynomial))])
-
             output_set_row = f_x(input_set)
-            if len(output_set_row) > 1:
-                output_set_row = (output_set_row - np.min(output_set_row)) / np.ptp(output_set_row)
-            else:
-                output_set_row = np.array([0.5])
             output_set = np.append(output_set, output_set_row.reshape(1, samples_number), axis=0)
+
+        output_set_range = np.ptp(output_set)
+        if abs(output_set_range) > 0.0000001:
+            output_set = (output_set - np.min(output_set)) / output_set_range
+        else:
+            output_set = output_set * 0 + 0.5
+
+        input_set = input_set.copy()
         input_set.resize(len(self.__function_store), samples_number)
         return TestingSet([input_set, output_set])
 

MLPApproximatorTest/test_functionGenerator.py
@@ -3,6 +3,8 @@
 
 from unittest import TestCase
 
+import numpy as np
+
 from MLPApproximator.MlpFunctionGenerator import FunctionGenerator
 
 
@@ -61,8 +63,7 @@ class TestFunctionGenerator(TestCase):
         testing_set = function_generator.generate(sample_number)
 
         for function_idx in range(function_number):
-            for sample_idx in range(sample_number):
-                self.assertEqual(2 * sample_idx + base_factor, testing_set.Y[function_idx][sample_idx])
+            self.assertTrue(np.all(np.array([0, 0.5, 1] == testing_set.Y[function_idx])))
 
     def test_generate100ContinuousSamples(self):
         function_generator = FunctionGenerator()
@@ -74,4 +75,4 @@ class TestFunctionGenerator(TestCase):
 
         first_function_idx = 0
         for sample_idx in range(sample_number):
-            self.assertEqual(sample_idx, testing_set.Y[first_function_idx][sample_idx])
+            self.assertAlmostEqual(sample_idx/sample_number, testing_set.Y[first_function_idx][sample_idx], delta=0.011)