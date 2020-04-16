02ce96a15efc79349e766ead8134e77bf338375c
Half's Inner <kajbrz@hotmail.com>
2020-04-16 11:08:21 +0200

    Adjust Chart Data

--- MLPApproximator/MlpUtils.py
+++ MLPApproximator/MlpUtils.py
@@ -73,12 +73,16 @@ class MlpUtils:
         :param to_file:
         """
         plt.plot(fitting_set_x.T[0], fitting_set_y.T[0], 'b-', label='F1 Expected')
-        plt.plot(fitting_set_x.T[0], learned_outputs.T[0], 'y-', label='F1 Predicted')
+        plt.plot(fitting_set_x.T[0], learned_outputs.T[0], 'y-',
+                 label='F1 Predicted {:2.3}%'.format(metrics.MeanSquaredErrors[0][-1] * 100))
         plt.plot(fitting_set_x.T[1], fitting_set_y.T[1], 'g-', label='F2 Expected')
-        plt.plot(fitting_set_x.T[1], learned_outputs.T[1], 'r-', label='F2 Predicted')
+        plt.plot(fitting_set_x.T[1], learned_outputs.T[1], 'r-',
+                 label='F2 Predicted {:2.3}%'.format(metrics.MeanSquaredErrors[1][-1] * 100))
         plt.plot(fitting_set_x.T[2], fitting_set_y.T[2], 'k-', label='F3 Expected')
-        plt.plot(fitting_set_x.T[2], learned_outputs.T[2], 'm-', label='F3 Predicted')
+        plt.plot(fitting_set_x.T[2], learned_outputs.T[2], 'm-',
+                 label='F2 Predicted {:2.3}%'.format(metrics.MeanSquaredErrors[2][-1] * 100))
         plt.xlabel('FIT ' + plot_name + ' {:2.3}%'.format(np.mean(metrics.AvgMeanSquaredError) * 100))
+
         plt.ylim(-0.1, 1.1)
         plt.legend()
         if to_file:
@@ -106,6 +110,8 @@ class MlpUtils:
         plt.plot(np.ascontiguousarray(np.arange(epoch_number)), metrics.MeanSquaredErrors[2], 'g-',
                  label='F3 RMSE')
         plt.plot(np.ascontiguousarray(np.arange(epoch_number)), metrics.AvgMeanSquaredError, 'm-',
+                 label='Avg RMSE')
+        plt.plot(np.ascontiguousarray(np.arange(epoch_number)), metrics.AvgMeanSquaredError, '-',
                  label='Avg RMSE')
         plt.xlabel('FIT ' + plot_name)
         plt.ylim(0, np.max(metrics.MeanSquaredErrors) * 1.1)
